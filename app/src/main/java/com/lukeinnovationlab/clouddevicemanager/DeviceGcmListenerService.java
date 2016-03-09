0a9f54d338a57ee240a626353570312b2847a807
Luke Chen <lchen.dev@gmail.com>
2016-03-08 23:20:08 -0500

    Story 1 - Initial support 6

--- app/src/main/java/com/lukeinnovationlab/clouddevicemanager/DeviceGcmListenerService.java
+++ app/src/main/java/com/lukeinnovationlab/clouddevicemanager/DeviceGcmListenerService.java
@@ -1,9 +1,45 @@
 package com.lukeinnovationlab.clouddevicemanager;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import com.google.android.gms.gcm.GcmListenerService;
 
 /**
  * Service handles GCM messages.
  */
 public class DeviceGcmListenerService extends GcmListenerService {
+    private static final String TAG = "DeviceGcmLS";
+
+    /**
+     * Called when message is received.
+     *
+     * @param from SenderID of the sender.
+     * @param data Data bundle containing message data as key/value pairs.
+     *             For Set of keys use data.keySet().
+     */
+    // [START receive_message]
+    @Override
+    public void onMessageReceived(String from, Bundle data) {
+        String message = data.getString("message");
+        Log.d(TAG, "From: " + from);
+        Log.d(TAG, "Message: " + message);
+
+        if (from.startsWith("/topics/")) {
+            // message received from some topic.
+        } else {
+            // normal downstream message.
+        }
+
+        // [START_EXCLUDE]
+        /**
+         * Production applications would usually process the message here.
+         * Eg: - Syncing with server.
+         *     - Store message in local database.
+         *     - Update UI.
+         */
+
+        // [END_EXCLUDE]
+    }
+    // [END receive_message]
 }
